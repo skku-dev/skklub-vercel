fefd03a28255ad6a5e0d7da29b85d2184f3a101d
skku-dev <>
2024-03-21 08:45:39 +0000

    add image to metadata

--- app/layout.js
+++ app/layout.js
@@ -5,36 +5,37 @@ import Header from '@/app/components/common/Header/Header';
 import RecoilRootProvider from './RecoilRootProvider';
 // Vercel Analytics
 import { Analytics } from '@vercel/analytics/react';
-
+const BASIC_THUMBNAIL = "@/public/assets/images/skklub.png";
 export const metadata = {
-	generator: 'SKKUD',
-	title: 'SKKLUB',
-	description:
-		'SKKLUB은 우리 학교의 모든 동아리/학회/단체를 한눈에 볼 수 있는 플랫폼입니다',
-	applicationName: 'SKKLUB',
-	referrer: 'origin-when-cross-origin',
-	keywords: ['Next.js', 'Spring', 'SKKUD', 'SKKU', 'SKKLUB'],
-	authors: [
-		{ name: 'SKKUD', url: 'https://skku.dev' },
-		{ name: 'SKKLUB', url: 'https://skklub-vercel.vercel.app/' },
-	],
-	creator: 'SKKUD',
-	publisher: 'SKKUD',
-	openGraph: {
-		title: 'SKKLUB',
-		description: '성균관대학교 동아리들을 한번에 볼 수 있는 플랫폼, 스클럽.',
-		url: 'https://skklub-vercel.vercel.app/',
-		siteName: 'SKKLUB',
-		images: [
-			{
-				url: '/assets/images/skklub.png',
-				width: 3152,
-				height: 1776,
-			},
-		],
-		locale: 'en_US',
-		type: 'website',
-	},
+  generator: "SKKUD",
+  title: "SKKLUB",
+  description:
+    "SKKLUB은 우리 학교의 모든 동아리/학회/단체를 한눈에 볼 수 있는 플랫폼입니다",
+  image: BASIC_THUMBNAIL,
+  applicationName: "SKKLUB",
+  referrer: "origin-when-cross-origin",
+  keywords: ["Next.js", "Spring", "SKKUD", "SKKU", "SKKLUB"],
+  authors: [
+    { name: "SKKUD", url: "https://skku.dev" },
+    { name: "SKKLUB", url: "https://skklub-vercel.vercel.app/" },
+  ],
+  creator: "SKKUD",
+  publisher: "SKKUD",
+  openGraph: {
+    title: "SKKLUB",
+    description: "성균관대학교 동아리들을 한번에 볼 수 있는 플랫폼, 스클럽.",
+    url: "https://skklub-vercel.vercel.app/",
+    siteName: "SKKLUB",
+    images: [
+      {
+        url: "/assets/images/skklub.png",
+        width: 3152,
+        height: 1776,
+      },
+    ],
+    locale: "en_US",
+    type: "website",
+  },
 };
 
 export default function RootLayout({ children }) {
